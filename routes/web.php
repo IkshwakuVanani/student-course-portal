a1193490a6a17df749a1f7894a5833bb86a68fe4
Ikshwaku V <74739931+IkshwakuVanani@users.noreply.github.com>
2025-04-30 03:57:09 -0400

    Delete routes directory

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CourseController;
-
-Route::get('/', [CourseController::class, 'index']);       // List all courses
-Route::resource('courses', CourseController::class);       // CRUD endpoints
