769086e694fa1287906d9ba3f532bde217d882ad
Ikshwaku V <74739931+IkshwakuVanani@users.noreply.github.com>
2025-04-30 03:58:42 -0400

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
+
+Route::get('/', [CourseController::class, 'index'])->name('home');
+Route::resource('courses', CourseController::class);
